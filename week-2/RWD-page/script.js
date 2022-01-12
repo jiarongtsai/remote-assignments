d7b4d01e96a23109ff910446b3c7c85fb28e89f7
Jia Rong Tsai <jiarongtsai19@gmail.com>
2022-01-12 15:35:10 +0800

    feat: 1.toggle welcome message when click. 2. add show more content boxes when click.

--- week-2/RWD-page/script.js
+++ week-2/RWD-page/script.js
@@ -1,17 +1,45 @@
 const welcomeDiv = document.querySelector('.welcome')
 const welcomeMsg = welcomeDiv.children[0]
 
+const showButton = document.querySelector('#showContentBox')
+const contentBoxWrapper_fixed = document.querySelectorAll('.content-box-wrapper')[1] 
+
 const addButton = document.querySelector('#addContentBox')
 const contentBoxWrapper = document.querySelector('.content-box-wrapper')
 
-let newContentBox, newText, count
+let newContentBox, newText, count, showContent, trackWelcome
 count = 5
+showContent = false
+trackWelcome = true
 
+//rq 1
 function changeText(){
-    //could I toggle the text message
-    welcomeMsg.innerHTML = "Have a Good Time!"
+    if(trackWelcome){
+        welcomeMsg.innerHTML = "Have a Good Time!"
+        trackWelcome = false
+    }else{
+        welcomeMsg.innerHTML = "Welcome Message"
+        trackWelcome = true
+    }   
 }
 
+//rq 2
+
+//rq 3
+function toggleContentBox(){
+    contentBoxWrapper_fixed.classList.toggle('display-none')
+    
+    if(!showContent){
+        showButton.innerText = 'Hide Boxes'
+        showContent = true
+    }else{
+        showButton.innerText = 'Show More Boxes'
+        showContent = false
+    }
+    
+}
+
+//rq 3.5
 function addContentBox(){
     //create a new Content Box
     newContentBox = document.createElement('div')
@@ -29,7 +57,6 @@ function addContentBox(){
 }
 
 
-
-
 welcomeDiv.addEventListener('click', changeText)
+showButton.addEventListener('click', toggleContentBox)
 addButton.addEventListener('click', addContentBox)
