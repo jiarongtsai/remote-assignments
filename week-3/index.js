cf059a673a2505ff959c96018402d43de0e66d54
Jia Rong Tsai <jiarongtsai19@gmail.com>
2022-01-20 11:50:23 +0800

    feat: week3-assignemnt2 | simple API (handle GET request and parameters)

--- week-3/index.js
+++ week-3/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
+
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send('Hello World')
+    res.send('Hello, My Server!')
 })
+
+app.get('/getData', (req, res)=>{
+    //get the query sting
+    const input = req.query.number
+    const num = Number(input)
+    
+    //main logic
+    if(!input)
+        res.send('Lack of Parameter')
+    if(num > 0){
+        let sum =0
+        for(let i = 0; i<num; i++){
+            sum+=i
+        }
+        //res.send only accept string data
+        res.send(sum.toString())
+    }
+    res.send('Wrong Parameter')
+        
+})
+
 
 app.listen(3000, ()=>{
     console.log('The server is listening on port 3000.')
