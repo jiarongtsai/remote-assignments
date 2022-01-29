c6f3b28cd1e4bbbeefb1a74b3ac0af297dbc29e8
Jia Rong Tsai <jiarongtsai19@gmail.com>
2022-01-29 20:06:57 +0800

    fix: week4 assignment2 | replace innerHTML with textContent

--- week-4/assignment2/assignment2.js
+++ week-4/assignment2/assignment2.js
@@ -1,49 +1,40 @@
+const myDiv = document.getElementById("myDiv")
 
-/*
 function ajax(src, callback){
-    // your code here
+    const xhr = new XMLHttpRequest()
+    xhr.onreadystatechange =()=>{
+        if (xhr.readyState === 4) {  
+            if (xhr.status === 200) {
+                const data = JSON.parse(xhr.responseText)
+                callback(data)
+            }
+        }
+    }
+    xhr.open('GET', src)
+    xhr.send()
 }
 function render(data){
-    // your code here.
-    // document.createElement() and appendChild() are preferred. No innerHTML or 
-    something alike
+    data.forEach( async(element) => {
+        const container = document.createElement('div')
+        const name = document.createElement('h1')
+        const description = document.createElement('p')
+        const price = document.createElement('span')
+
+        name.textContent = await element.name
+        description.textContent = await element.description
+        price.textContent = await element.price
+
+        container.append(name, description, price)
+        myDiv.appendChild(container) 
+    })
 } 
     
 ajax(
     "https://appworks-school.github.io/Remote-Aassigiment-Data/products", 
     function(response){
-        render(response); 
+        render(response)
     }
-); // you should get product information in JSON format and render data in the page
-*/
+)
 
-const myDiv = document.getElementById("myDiv")
 
-const xhr = new XMLHttpRequest()
 
-xhr.onreadystatechange = ()=>{
-    if (xhr.readyState === 4) {  
-        if (xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText)
-            const myDiv = document.querySelector('#myDiv')
-            
-            data.forEach( async(element) => {
-
-                const container = document.createElement('div')
-                const name = document.createElement('h1')
-                const description = document.createElement('p')
-                const price = document.createElement('span')
-
-                name.innerHTML = await element.name
-                description.innerHTML = await element.description
-                price.innerHTML = await element.price
-
-                container.append(name, description, price)
-                myDiv.append(container) 
-            });
-
-        }
-    }
-}
-xhr.open('GET', "https://appworks-school.github.io/Remote-Aassigiment-Data/products")
-xhr.send()
