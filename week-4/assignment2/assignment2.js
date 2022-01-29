1163add074b73f6abd6853541a2c80abb99d6855
Jia Rong Tsai <jiarongtsai19@gmail.com>
2022-01-29 19:15:55 +0800

    feat: week4 assignment2 | Ajax request with XML

--- week-4/assignment2/assignment2.js
+++ week-4/assignment2/assignment2.js
@@ -2,22 +2,11 @@
 /*
 function ajax(src, callback){
     // your code here
-    const xhr = new XMLhttpRequest
-    xhr.onreadystatechange = ()=>{
-        if (xhr.readyState === 4) {   // xhrRequest.DONE == 4
-            if (xhr.status === 200) {
-                document.getElementById("myDiv").innerHTML = xhr.responseText;
-            }
-         }
-    }
-    xhr.open('GET', src)
-    xhr.send()
 }
 function render(data){
     // your code here.
     // document.createElement() and appendChild() are preferred. No innerHTML or 
-    // something alike
-    console.log(data)
+    something alike
 } 
     
 ajax(
@@ -28,13 +17,16 @@ ajax(
 ); // you should get product information in JSON format and render data in the page
 */
 
+const myDiv = document.getElementById("myDiv")
 
 const xhr = new XMLHttpRequest()
 
 xhr.onreadystatechange = ()=>{
     if (xhr.readyState === 4) {  
         if (xhr.status === 200) {
-            document.getElementById("myDiv").innerHTML = xhr.responseText
+            const data = JSON.parse(xhr.responseText)
+            console.log(data)
+            myDiv.innerHTML = data[0]
         }
     }
 }
