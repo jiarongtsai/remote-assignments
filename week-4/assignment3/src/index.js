29425f06abea2888b2d3ef5fe7d075337d6cdd2b
Jia Rong Tsai <jiarongtsai19@gmail.com>
2022-01-29 22:43:29 +0800

    feat: week4-assignemnt3 | Toggle Text, Menu, and Boxed change

--- week-4/assignment3/src/index.js
+++ week-4/assignment3/src/index.js
@@ -1,67 +1,97 @@
-const App =()=>{
-    return(
-        <div className="body">
-            <header>
-                <nav>
-                    <a href="/" className="nav-logo">Web Title / Logo</a>
+class App extends React.Component{
+    state = {
+        toggleMainText: false,
+        showMenu: false,
+        showBox: true
+    }
 
-                    <input type="checkbox" id="hamburger"/>
-                    <label htmlFor="hamburger">&equiv;</label>
+    handleMainTextChange=()=>{
+        this.setState({
+            toggleMainText: !this.state.toggleMainText
+        })
+    }
 
-                    <ul className="nav-links display-none">
-                        <li className="nav-item"><a href="/">item 1</a></li>
-                        <li className="nav-item"><a href="/">item 2</a></li>
-                        <li className="nav-item"><a href="/">item 3</a></li>
-                        <li className="nav-item"><a href="/">item 4</a></li>
-                    </ul>
-                </nav>       
-            </header>
+    handleMenuChange=()=>{
+        this.setState({
+            showMenu: !this.state.showMenu
+        })
+    }
 
-            <section className="welcome">
-                <h1>Welcome Message</h1>
-            </section>
+    handleBoxShow=()=>{
+        this.setState({
+            showBox: !this.state.showBox
+        })
+    }
 
-            <section className="content">
-                <h2>Section Title</h2>
-                <div className="content-box-wrapper">
-                    <div className="content-box">
-                        <p>Content Box 1</p>
+    render(){
+        return(
+            <div className="body">
+                <header>
+                    <nav>
+                        <a href="/" className="nav-logo">Web Title / Logo</a>
+
+                        <input type="checkbox" id="hamburger" onClick={this.handleMenuChange}/>
+                        <label htmlFor="hamburger">{this.state.showMenu ? "×":"≡"}</label>
+
+                        <ul className={this.state.showMenu ? "nav-links":"nav-links display-none"}>
+                            <li className="nav-item"><a href="/">item 1</a></li>
+                            <li className="nav-item"><a href="/">item 2</a></li>
+                            <li className="nav-item"><a href="/">item 3</a></li>
+                            <li className="nav-item"><a href="/">item 4</a></li>
+                        </ul>
+                    </nav>       
+                </header>
+
+                <Welcome 
+                    toggleMainText={this.state.toggleMainText}
+                    handleMainTextChange={this.handleMainTextChange}
+                />
+                
+                <section className="content">
+                    <h2>Section Title</h2>
+                    <div className={this.state.showBox ? "content-box-wrapper":"content-box-wrapper display-none"}>
+                        <div className="content-box">
+                            <p>Content Box 1</p>
+                        </div>
+                        <div className="content-box">
+                            <p>Content Box 2</p>
+                        </div>
+                        <div className="content-box">
+                            <p>Content Box 3</p>
+                        </div>
+                        <div className="content-box">
+                            <p>Content Box 4</p>
+                        </div>
                     </div>
-                    <div className="content-box">
-                        <p>Content Box 2</p>
-                    </div>
-                    <div className="content-box">
-                        <p>Content Box 3</p>
-                    </div>
-                    <div className="content-box">
-                        <p>Content Box 4</p>
-                    </div>
-                </div>
-                <button id = "addContentBox">Add a Content Box</button>
-                <hr />
-                <button id = "showContentBox">Show More Boxes</button>
-                <div className="content-box-wrapper display-none">
-                    <div className="content-box">
-                        <p>Content Box 1</p>
-                    </div>
-                    <div className="content-box">
-                        <p>Content Box 2</p>
-                    </div>
-                    <div className="content-box">
-                        <p>Content Box 3</p>
-                    </div>
-                    <div className="content-box">
-                        <p>Content Box 4</p>
-                    </div>
-                </div>
-            </section>
-            
-            <footer>
-                <p><small>Developed with love by Bella &copy 2022</small></p>
-            </footer>
-        </div>
-    )
+                    <button id = "showContentBox" onClick={this.handleBoxShow}>Toggle Boxes Show</button>
+                </section>
+                
+                <footer>
+                    <p><small>Developed with love by Bella &copy 2022</small></p>
+                </footer>
+            </div>
+        )
+    }   
 }
+
+
+const Welcome =(props)=>{
+    if(props.toggleMainText){
+        return(
+            <section className="welcome">
+                <h1 onClick={props.handleMainTextChange}>Welcome Message</h1>
+            </section>
+        )
+    }else{
+        return(
+            <section className="welcome">
+                <h1 onClick={props.handleMainTextChange}>Have a good day</h1>
+            </section>
+        )
+    }
+    
+}
+
 ReactDOM.render(
     <App />,
     document.getElementById('root')
